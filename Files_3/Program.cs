a6c5b9e8b8af93b01db06b825ddb22872ac9709f
Asrorbek Abrorov <138248626+Asrorbek-Abrorov@users.noreply.github.com>
2024-01-07 13:06:30 +0500

    Math.Round with String, input String and int, output string

---
+++ Files_3/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Numerics;
+
+string Round(string x, int a)
+{
+    if (a <= 15)
+    {
+        return "Math.Round(x, a).ToString()";
+    }
+    else
+    {
+        var lst = x.ToString().Split('.');
+        var chars = lst[1].ToCharArray();
+        string result = $"{lst[0]}.";
+        if (chars[a] >= 5)
+        {
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (i == a - 1)
+                {
+                    chars[i] = (char)(chars[i] + 1);
+                    result += chars[i].ToString();
+                }
+                else if(i == a)
+                {
+                    result += 0;
+                }
+                else
+                {
+                    result += chars[i].ToString();
+                }
+            }
+            return (result);
+        }
+        else
+        {
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if(i == a)
+                {
+                    result += 0;
+                }
+                else
+                {
+                    result += chars[i].ToString();
+                }
+            }
+            return (result);
+        }
+    }
+}
+
+Console.WriteLine(Round("1.1111111111111117889777777", 20));
